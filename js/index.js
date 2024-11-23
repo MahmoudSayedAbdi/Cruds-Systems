13de1b06682fa7180b666199b1bd7fa43ea24cb0
MahmoudSayedAbdi <mahmoudsayed.2003.8@gmail.com>
2024-11-23 23:40:49 +0200

    display & delete row

--- js/index.js
+++ js/index.js
@@ -4,9 +4,11 @@ var taxes = document.getElementById("taxes");
 var ads = document.getElementById("ads");
 var discound = document.getElementById("discound");
 var total = document.getElementById("total");
+var count = document.getElementById("count")
 var category = document.getElementById("category");
 var create = document.getElementById("create");
 
+var tBody = document.querySelector(".outputs tbody")
 
 
 // get total 
@@ -47,12 +49,13 @@ function addProdect(){
         ads : ads.value,
         total :total.innerHTML,
         discound : discound.value,
+        count : count.value,
         category :category.value,
     } ;
     prodectList.push(prodect);
     localStorage.setItem("dataList", JSON.stringify(prodectList))
-    console.log(prodectList);
     clearForm()
+    display()
     
 }
 
@@ -65,5 +68,41 @@ function clearForm (){
     ads.value = "";
     total.innerHTML = "";
     discound.value = "";
+    count.value = ""
     category.value = "";
 };
+
+
+// read 
+
+function display() {
+    temp =``;
+    for(var i=0 ;i<prodectList.length ; i++){
+        temp += `
+            <tr>
+                <td>${i}</td>
+                <td>${prodectList[i].title}</td>
+                <td>${prodectList[i].price}</td>
+                <td>${prodectList[i].taxes}</td>
+                <td>${prodectList[i].ads}</td>
+                <td>${prodectList[i].discound}%</td>
+                <td>${prodectList[i].total}</td>
+                <td>${prodectList[i].category}</td>
+                <th><button >update</button></th>
+                <th><button onclick = "deleteElement(${i})">delete</button></th>
+            </tr>
+        `
+    }
+
+    tBody.innerHTML = temp
+}
+display()
+
+
+// delete row
+
+function deleteElement(i){
+    prodectList.splice(i,1);
+    localStorage.setItem("dataList", JSON.stringify(prodectList))
+    display()
+}
