dd5bc8938661f30f6ca289a802cdc969297a2e7b
MahmoudSayedAbdi <mahmoudsayed.2003.8@gmail.com>
2024-11-24 23:45:19 +0200

    upduteElement & delete row

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+// input
 var title = document.getElementById("title");
 var price = document.getElementById("price");
 var taxes = document.getElementById("taxes");
@@ -6,9 +7,15 @@ var discound = document.getElementById("discound");
 var total = document.getElementById("total");
 var count = document.getElementById("count")
 var category = document.getElementById("category");
+// Btn
 var create = document.getElementById("create");
-
+var deleteAll = document.getElementById("deleteAll");
+// body
 var tBody = document.querySelector(".outputs tbody")
+
+// global var
+var mood = "create"
+var temp ;
 
 
 // get total 
@@ -52,7 +59,22 @@ function addProdect(){
         count : count.value,
         category :category.value,
     } ;
-    prodectList.push(prodect);
+
+    if(mood == "create"){
+        if(prodect.count >1){
+            for(var i =0 ; i< prodect.count ; i++){
+                prodectList.push(prodect);
+            }
+            }
+            else{
+                prodectList.push(prodect);            
+            }
+    }else{
+        prodectList[temp] = prodect
+        create.innerHTML = "Create"
+        count.style.display = "block"
+        mood = "create"
+    }
     localStorage.setItem("dataList", JSON.stringify(prodectList))
     clearForm()
     display()
@@ -76,6 +98,8 @@ function clearForm (){
 // read 
 
 function display() {
+    getTotal()
+    // to display element
     temp =``;
     for(var i=0 ;i<prodectList.length ; i++){
         temp += `
@@ -88,13 +112,21 @@ function display() {
                 <td>${prodectList[i].discound}%</td>
                 <td>${prodectList[i].total}</td>
                 <td>${prodectList[i].category}</td>
-                <th><button >update</button></th>
+                <th><button onclick = "upduteElement(${i})" >update</button></th>
                 <th><button onclick = "deleteElement(${i})">delete</button></th>
             </tr>
         `
     }
-
     tBody.innerHTML = temp
+
+    // to display btn delete all and count num element
+    if(prodectList.length > 0){
+        deleteAll.style.display = "block"
+        deleteAll.innerHTML = `delete all (${prodectList.length})`
+    }else {
+        deleteAll.style.display = "none"
+    }
+
 }
 display()
 
@@ -105,4 +137,31 @@ function deleteElement(i){
     prodectList.splice(i,1);
     localStorage.setItem("dataList", JSON.stringify(prodectList))
     display()
+}
+deleteAll.addEventListener("click", deleteAllElement)
+function deleteAllElement(){
+    prodectList.splice(0);
+    localStorage.setItem("dataList", JSON.stringify(prodectList))
+    display()
+} 
+
+
+// Updute Func 
+
+function upduteElement(i){
+    title.value = prodectList[i].title;
+    price.value = prodectList[i].price;
+    taxes.value = prodectList[i].taxes;
+    ads.value = prodectList[i].ads;
+    discound.value = prodectList[i].discound;
+    getTotal();
+    category.value = prodectList[i].category;
+    count.style.display = "none";
+    create.innerHTML = "Updute";
+    mood = "updute";
+    temp = i;
+    scroll({
+        top:0,
+        behavior:"smooth"
+    })
 }
